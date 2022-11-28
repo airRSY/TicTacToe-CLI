049f5d5c155ff13f2112b94488e0936e7683dd13
AirRSY <rasyajusticio.public@gmail.com>
2022-11-28 16:51:45 +0700

    1.5

--- game/game.py
+++ game/game.py
@@ -1,11 +1,12 @@
 # --- MAIN CLASS ---
-class GameClass():
+class GameClass:
     def __init__(self) -> None:
+        self.cli_class = None
         self.user_input = -1
         self.current_player = 1
         self.is_game_running = True
         self.marked_columns = 0
-        self.wins = (0, 0)
+        self.wins = [0, 0]
         self.information_code = 0
         self.board = [
             [0, 0, 0],
@@ -15,10 +16,10 @@ class GameClass():
     
     # -- PUBLIC METHODS --
     def get_input_send_request(self) -> bool:
-        if (self.is_game_running):
+        if self.is_game_running:
             self.cli_class.get_input()
             succes = self.verify_input()
-            if (succes):
+            if succes:
                 row, column = self.format_input()
                 self.verify_mark_columns(row, column, self.current_player)
             
@@ -30,14 +31,14 @@ class GameClass():
     def set_game_state(self) -> None:
         state = self.get_board_state()
         self.information_code = state
-        if (state != 0):
+        if state != 0:
             self.is_game_running = False
             self.cli_class.display_cli()
-            if (state == 1):
+            if state == 1:
                 self.wins[0] += 1
-            elif (state == 2):
+            elif state == 2:
                 self.wins[1] += 1
-    
+
     def reset(self) -> None:
         self.user_input = -1
         self.current_player = 1
@@ -52,32 +53,29 @@ class GameClass():
     
     # -- PRIVATE METHODS --
     def verify_mark_columns(self, row, column, player_code) -> None:
-        if (self.board[row][column] == 0) and (player_code != 0):
-            print("Yes")
+        if self.board[row][column] == 0 and player_code != 0:
             self.mark_column(row, column, player_code)
             self.next_turn()
-        else:
-            print("No")
     
     def get_board_state(self) -> int:
         # Horizontal
         for row in range(3):
-            if (self.board[row][0] == self.board[row][1] == self.board[row][2] != 0):
+            if self.board[row][0] == self.board[row][1] == self.board[row][2] != 0:
                 return self.board[row][1]
         
         # Vertical
         for column in range(3):
-            if (self.board[0][column] == self.board[1][column] == self.board[2][column] != 0):
+            if self.board[0][column] == self.board[1][column] == self.board[2][column] != 0:
                 return self.board[1][column]
         
         # Diagonal
-        if (self.board[0][0] == self.board[1][1] == self.board[2][2] != 0):
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
             return self.board[1][1]
-        if (self.board[0][2] == self.board[1][1] == self.board[2][0] != 0):
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
             return self.board[1][1]
         
         # Draw
-        if (self.is_board_full()):
+        if self.is_board_full():
             return 3
         
         # Playing
@@ -87,37 +85,37 @@ class GameClass():
         self.current_player = self.current_player % 2 + 1
         self.information_code = 0
     
-    def is_board_full(self) -> None:
+    def is_board_full(self) -> bool:
         return self.marked_columns >= 9
 
     def format_input(self):
         input_ = self.user_input
         
-        if (input_ == 1):
+        if input_ == 1:
             return 0, 0
-        if (input_ == 2):
+        if input_ == 2:
             return 0, 1
-        if (input_ == 3):
+        if input_ == 3:
             return 0, 2
-        if (input_ == 4):
+        if input_ == 4:
             return 1, 0
-        if (input_ == 5):
+        if input_ == 5:
             return 1, 1
-        if (input_ == 6):
+        if input_ == 6:
             return 1, 2
-        if (input_ == 7):
+        if input_ == 7:
             return 2, 0
-        if (input_ == 8):
+        if input_ == 8:
             return 2, 1
-        if (input_ == 9):
+        if input_ == 9:
             return 2, 2
         return -1, -1
         
     def verify_input(self) -> bool:
-        if (self.user_input < 1) or (self.user_input > 9):
+        if self.user_input < 1 or self.user_input > 9:
             return False
         return True
     
-    def mark_column(self, row, column, player_code) -> bool:
+    def mark_column(self, row, column, player_code) -> None:
         self.board[row][column] = player_code
         self.marked_columns += 1
--- run.py
+++ run.py
@@ -1,33 +1,37 @@
 # --- MODULES ---
 from game.game import GameClass
 from cli.cli import CliClass
+
+
 # -- INIT MODULES --
 Game = GameClass()
 CLI = CliClass(Game)
 Game.set_cli_class(CLI)
 
+
 # --- MAIN CODE ---
 # -- MAIN FUNCTIONS --
-def restartGame() -> bool:
+def restart_game() -> bool:
     yes = ("iya", "ya", "ulang", "kembali", "y", "mengulang", "yes")
-    
-    CLI.new_line()
+
+    print()
     print("Ingin mengulang permainan?")
-    resetInput = str(input(">> "))
-    
-    if (resetInput in yes):
+    reset_input = str(input(">> "))
+
+    if reset_input in yes:
         Game.reset()
         Game.is_game_running = True
         return True
     else:
         return False
 
+
 # -- MAIN LOOP --
-while (True):
+while True:
     CLI.display_cli()
     if not (Game.get_input_send_request()):
         continue
     Game.set_game_state()
-    
-    if not (Game.is_game_running) and not (restartGame()):
+
+    if not Game.is_game_running and not restart_game():
         break
